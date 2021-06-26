c6535a2632459ce759d97ed9772310bd99cf8b56
Sahil Bambulkar <Sahilkbambulkar@gmail.com>
2021-06-26 13:17:21 -0400

    Crypto Currency Pricing SMS Notifier
    Uses Coinbase API to retrive crypto price data and Twilio API to text data through SMS messages to a phone. Automatically sends messages every 10 minutes by default but can be adjusted. Only accepts Coinbase supported currencies, if no command line arguments given will default to ETH and BTC. Note: all sensitive information is stored in enviroment variables, please set yours appropriately.

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+from twilio.rest import Client
+import requests
+import json
+import schedule
+import time
+import sys
+
+
+def PriceCheck(coin):
+     response = requests.get(f'https://api.coinbase.com/v2/prices/{coin}-USD/spot')
+     data = response.json()
+     cryptoInfo = data['data']
+     coin = cryptoInfo["base"]
+     currency = cryptoInfo["currency"]
+     val = cryptoInfo["amount"]
+     return f'{coin} is currently at {val} {currency}'
+
+
+def SendText(coinsToCheck):
+     m=""
+     for coin in coinsToCheck:
+          m+=PriceCheck(coin)+"\n"
+     account_sid = os.environ['TWILIO_ACCOUNT_SID']
+     auth_token = os.environ['TWILIO_AUTH_TOKEN']
+     client = Client(account_sid, auth_token)
+
+     message = client.messages \
+                     .create(
+                          body=m,
+                          from_=os.environ['TPN'],
+                          to=os.environ['PN']
+                 )
+
+
+def job():
+     SendText(['BTC','ETH'])
+
+
+if __name__ == "__main__":
+     coinBaseCryptos = ['ZRX', '1INCH', 'AAVE', 'ALGO', 'AMP', 'FORTH', 'ANKR', 'BAL', 'Token BNT', 'BAND', 'BOND', 'Token BAT', 'BTC', 'Cash BCH', 'ADA', 'CTSI', 'CGLD', 'CHZ', 'COMP', 'ATOM', 'Token CRV', 'DAI', 'COMP', 'DASH', 'COMP', 'DOGE', 'Coin ENJ', 'MLN', 'EOS', 'ETH', 'ETC', 'FIL', 'GTC', 'RLC', 'ICP', 'KEEP', 'KNC', 'LTC', 'LRC', 'MIR', 'NKN', 'NU', 'NMR', 'OMG', 'OXT', 'OGN', 'DOT', 'MATIC', 'REN', 'SKL', 'SOL', 'XLM', 'STORJ', 'SUSHI', 'SNX', 'TRB', 'USDT', 'XTZ', 'GRT', 'UMA', 'UNI', 'USDC', 'WTBC', 'YFI', 'ZEC']
+     eligibleCrpytos = [x for x in sys.argv if x in coinBaseCryptos]
+     if len(eligibleCrpytos)==0: eligibleCrpytos = ['ETH', 'BTC']
+
+
+schedule.every(10).minutes.do(job)
+
+while True:
+     schedule.run_pending()
+     time.sleep(1)
